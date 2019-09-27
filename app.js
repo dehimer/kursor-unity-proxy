6f48f8f267a1971db73f6b699ce84e9005093ebf
Denis Novikov <den-vse@yandex.ru>
2019-09-27 08:25:03 +0300

    feature: implement all logic

---
+++ app.js
@@ -0,0 +1,48 @@
+const request = require('request');
+const io = require('socket.io-client');
+const config = require('./config');
+
+const socket = io(config.herokuAppUrl);
+let answers = [];
+
+socket.on('connect', () => {
+  console.log('connect');
+
+  socket.emit('auth:submit', config.password);
+});
+
+socket.on('auth:success', () => {
+  console.log('auth:success');
+
+  socket.emit('answers:all');
+});
+
+socket.on('answers:all', (data) => {
+  console.log('answers:all');
+  answers = data;
+  console.log(answers);
+  // const sentAnswers = answers.filter((a) => a.sent);
+  // console.log('sentAnswers');
+  // console.log(sentAnswers);
+});
+
+socket.on('answers:new', (newAnswer) => {
+  console.log('answers:new');
+  console.log(newAnswer);
+  answers.push(newAnswer);
+});
+
+socket.on('answers:sent', (answerId) => {
+  console.log('answers:sent');
+  console.log(answerId);
+  const answer = answers.find((a) => a.id === answerId);
+  console.log('answer');
+  console.log(answer);
+  if (answer) {
+    request(config.unityUrl+'?message='+encodeURIComponent(answer.text), (error) => {
+      if (error) console.error(error);
+    });
+  } else {
+    console.error('Answer with id:' + answerId + ' was not found');
+  }
+});
---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  herokuAppUrl: 'http://kursor.space',
+  password: '123',
+  unityUrl: '192.168.1.65:1999'
+};
